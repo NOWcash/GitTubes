9935f6436d91d2150b14e39d2e072d1e6a3a2329
NOWcash <115737774+NOWcash@users.noreply.github.com>
2022-11-20 21:09:04 +0300

    Initialize development

---
+++ src/renderer/views/Playlist/Playlist.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ft-loader
+      v-if="isLoading"
+      :fullscreen="true"
+    />
+
+    <playlist-info
+      v-if="!isLoading"
+      :data="infoData"
+      class="playlistInfo"
+    />
+
+    <ft-card
+      v-if="!isLoading"
+      class="playlistItems"
+    >
+      <div
+        v-for="(item, index) in playlistItems"
+        :key="index"
+        class="playlistItem"
+      >
+        <p
+          class="videoIndex"
+        >
+          {{ index + 1 }}
+        </p>
+        <ft-list-video
+          :data="item"
+          :playlist-id="playlistId"
+          :playlist-index="index"
+          appearance="result"
+          force-list-type="list"
+        />
+      </div>
+    </ft-card>
+  </div>
+</template>
+
+<script src="./Playlist.js" />
+<style scoped src="./Playlist.css" />
